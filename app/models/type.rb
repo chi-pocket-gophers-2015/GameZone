98e99c2e1d5dbe2d671ac7fdc0e077eaa8d39c93
Nathaniel Clapp <github@nattyclapp.com>
2015-05-20 14:52:35 -0500

    add associations to models

--- app/models/type.rb
+++ app/models/type.rb
@@ -1,2 +1,4 @@
 class Type < ActiveRecord::Base
+  has_many :game_types
+  has_many :games, through: :game_types
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :ownerships
+  has_many :votes
+  has_many :comments
+  has_many :games, through: :ownerships
 end
