838d84407c27293c9d611deb960cf99e1505a87c
Nathaniel Clapp <github@nattyclapp.com>
2015-05-21 11:27:01 -0500

    pull game partial logic into two helper methods

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,10 @@
 module GamesHelper
+
+  def check_players(game)
+    game.players_min == game.players_max
+  end
+
+  def duration(game)
+    game.duration < 60 ? "#{game.duration} min." : pluralize(game.duration/60, "hour")
+  end
 end
