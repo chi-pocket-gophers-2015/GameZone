f326717d1ed53799759070e99e4ce629335c3311
fancyultrafresh <sestaszak@gmail.com>
2015-05-21 05:16:04 -0500

    tweaks, added formatting

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,8 +2,7 @@ class GamesController < ApplicationController
 
   def index
     @type = Type.find_by(id: params[:type_id])
-    game_type = Game_type.find_by(type: @type)
-    @games = Game.find_by(game_type: game_type)
+    @games = @type.games
   end
 
   def show
