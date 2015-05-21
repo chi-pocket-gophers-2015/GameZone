343ca5640715c4121b929d19202506957d082fae
Nathan Werbeckes <werbeckes@gmail.com>
2015-05-21 11:34:42 -0500

    sets session id when you successfully create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 
 		if @user.save
+			session[:user_id] = @user.id
 			redirect_to home_path
 		else
 			render 'form'
