74a612cbd22ad401222eb15203adedca7e2fb45c
fancyultrafresh <sestaszak@gmail.com>
2015-05-21 04:58:14 -0500

    first attempt, doesn't work yet

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,11 @@
 class GamesController < ApplicationController
 
+  def index
+    @type = Type.find_by(id: params[:type_id])
+    game_type = Game_type.find_by(type: @type)
+    @games = Game.find_by(game_type: game_type)
+  end
+
   def show
     @game = Game.find_by(id: params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,9 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-resources :games
+resources :types do
+    resources :games
+  end
   # Example resource route with options:
   #   resources :products do
   #     member do
