bd542182e96cd5a204e39219056ef8691e1bda35
Nathaniel Clapp <github@nattyclapp.com>
2015-05-21 13:37:50 -0500

    extract game partial logic to helpers

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -7,4 +7,17 @@ module GamesHelper
   def duration(game)
     game.duration < 60 ? "#{game.duration} min." : pluralize(game.duration/60, "hour")
   end
+
+  def i_own?(game)
+    if current_user.games.include?(game)
+
+    else
+      <td></td>
+    end
+  end
+
+  def friends_own?
+    if :friends_own
+  end
+
 end
