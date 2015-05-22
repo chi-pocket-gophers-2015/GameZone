4eea1ed67d0846d5946949458de3408077b745d9
Nathan Werbeckes <werbeckes@gmail.com>
2015-05-22 10:12:05 -0500

    comments are working

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,4 +5,16 @@ class Game < ActiveRecord::Base
   has_many :game_types
   has_many :types, through: :game_types
   has_many :users, through: :ownerships
+
+  def score
+  	# score = votes.pluck(:value).inject(:+)
+  	# if score
+  	# 	return score
+  	# else
+  	# 	return 0
+  	# end
+
+  	score = votes.pluck(:value).inject(:+) 
+  	score ? (return score) : (return 0)
+  end
 end
