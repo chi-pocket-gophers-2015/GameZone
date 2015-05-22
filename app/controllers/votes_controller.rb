d96f44d3db2aa5b614bfd6adbc8191a3af41d725
Nathan Werbeckes <werbeckes@gmail.com>
2015-05-22 15:00:10 -0500

    votinggit add .git add .

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,24 +1,3 @@
 class VotesController < ApplicationController
 
-	def upvote
-		register_vote(1)
-		redirect_to game_path(@game)
-	end
-
-	def downvote
-		register_vote(-1)
-		redirect_to game_path(@game)
-	end
-
-	private
-		def register_vote(new_value)
-			@game = Game.find_by_id(params[:game_id])
-			@vote = Vote.where(user: current_user, game: @game)
-
-			if @vote
-				@vote.value = new_value
-			else
-				@game.votes.new(user: current_user, value: new_value)
-			end
-		end
 end
