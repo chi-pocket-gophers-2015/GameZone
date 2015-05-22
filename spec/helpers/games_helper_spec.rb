56d274a5335d5e2b879369a9764f84d590e294e0
Nathaniel Clapp <github@nattyclapp.com>
2015-05-22 12:19:55 -0500

    work on games helper test, all but #i_own working

--- spec/helpers/games_helper_spec.rb
+++ spec/helpers/games_helper_spec.rb
@@ -10,6 +10,50 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe TypesHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe GamesHelper do
+  let!(:game1) { Game.create!(title: "Checkers", players_min: 2, players_max: 2, duration: 15, complexity: "low") }
+  let!(:game2) { Game.create!(title: "Sorry!", players_min: 2, players_max: 6, duration: 60, complexity: "low") }
+  let!(:brang) { User.create!(username: "brang", password: "brang") }
+  let!(:natty) { User.create!(username: "natty", password: "natty") }
+  let!(:ownership1) { Ownership.create!(user: brang, game: game1) }
+  let!(:ownership2) { Ownership.create!(user: natty, game: game1) }
+
+  describe "#check_players" do
+
+    it "return true when players_min equals players_max" do
+      expect(game1.players_min == game1.players_max).to be_truthy
+    end
+
+    it "return false when players_min doesn't equal players_max" do
+      expect(game2.players_min == game2.players_max).to be_falsey
+    end
+  end
+
+  describe "#duration" do
+    it "returns minutes for values under 60 minutes" do
+      expect(duration(game1)).to eq("15 min.")
+    end
+
+    it "returns hours for values 60 minutes and over" do
+      expect(duration(game2)).to eq("1 hour")
+    end
+  end
+
+  describe "#i_own?" do
+    it "presents `delete` button for games I own" do
+      # helper.stub(:current_user) { natty }
+      allow(helper).to receive(:current_user) { natty }
+      expect(i_own?(game1)).to be_truthy
+    end
+
+    xit "presents `add` button for games I don't own" do
+      expect(i_own?(game2)).to eq()
+    end
+  end
+
+  describe "#friends_own?" do
+    it "returns owners of a game" do
+      expect(friends_own?(game1)).to eq("brang, natty")
+    end
+  end
 end
